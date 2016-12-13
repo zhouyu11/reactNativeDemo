e1356ef0b875401d13f5430d3fc55873d67d0c32
Zhou Yu <zhouyu@SS-NB-L0129.local>
2016-12-13 18:27:13 +0800

    add http util

--- js/Pages/LoginPage/LoginPage.js
+++ js/Pages/LoginPage/LoginPage.js
@@ -1,28 +1,61 @@
-'use strict'
+'use strict';
 
-import React from 'react'
-import { Text, TouchableHighlight, TextInput, View } from 'react-native'
+import React from 'react';
+import { Text, TouchableHighlight, TextInput, View, StyleSheet } from 'react-native';
+import Button from 'react-native-button';
+import HttpUtils from '../../Utils/HttpUtils';
 
 export class LoginPage extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       password: ''
-    }
+    };
+
+    this._handlePress = this._handlePress.bind(this);
+    this._handleLogIn = this._handleLogIn.bind(this);
   }
   render() {
     return (
-      <View>
-        <Text>Login</Text>
-        <View>
+      <View style={styles.view}>
+        <Text style={styles.title}>Login</Text>
+        <View style={styles.content}>
           <Text>
             password:
           </Text>
           <TextInput style={{height: 40, borderColor: 'gray', borderWidth: 1}} onChangeText={(text) => this.setState({password: text})} value={this.state.password} />
+          <Button
+            style={{fontSize: 20, color: 'green'}}
+            styleDisabled={{color: 'red'}}
+            onPress={() => this._handleLogIn()}>
+            Log In!
+          </Button>
         </View>
       </View>
     )
   }
+
+  _handlePress(){
+    this.setState({password: 'log in'});
+  }
+
+  _handleLogIn(){
+    let httpUtils = new HttpUtils();
+    httpUtils.init().setUrl('http://gank.io/api/random/data/Android/20').setMethod('GET').dofetch().then((data) => {this.setState({password: 'ok'})}).catch((err) => {this.setState({password: 'err'})})
+  }
 }
+
+const styles = StyleSheet.create({
+  view : {
+    flex:1,
+  },
+  title: {
+    flex:1,
+    alignSelf: 'center'
+  },
+  content:{
+    flex:10,
+  }
+})
 
 export default LoginPage;
